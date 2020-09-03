479d9615120c1789b81abe0e73ae0d90c977362d
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-03 17:58:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,7 +46,7 @@ class Owner
   end
   
   def list_pets
-    "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
   def self.all
