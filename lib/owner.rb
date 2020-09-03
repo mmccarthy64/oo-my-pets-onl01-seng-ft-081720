9c3c3f71b1801d8626ee9112ab8606b722eefea3
Matthew McCarthy <mmccarthy64@hotmail.com>
2020-09-03 17:56:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,64 @@
 class Owner
   # code goes here
+  attr_accessor :dog, :cat
+  attr_reader :species, :name
+  
+  @@all = []
+  @@pets = {:dogs => [], :cats => []}
+  
+  def initialize(species)
+    species = "human"
+    @species = species
+    @name = name
+    @@all << self
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def cats
+    
+  end
+  
+  def dogs
+    
+  end
+  
+  def buy_cat(name)
+    
+  end
+  
+  def buy_dog(name)
+    
+  end
+  
+  def walk_dog
+    
+  end
+  
+  def feed_cat
+    
+  end
+  
+  def sell_pets
+    
+  end
+  
+  def list_pets
+    "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+
 end
